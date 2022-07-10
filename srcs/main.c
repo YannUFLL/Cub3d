ea8a5a3194d5a779712dd28fd1d46028bccaab57
Yann Dumaine <yannlescoul79f>
2022-07-10 18:59:04 +0200

    un truc apparait a lecran

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 13:38:35 by ydumaine          #+#    #+#             */
-/*   Updated: 2022/07/08 19:52:16 by ydumaine         ###   ########.fr       */
+/*   Updated: 2022/07/10 18:52:22 by ydumaine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,6 +74,62 @@ int ft_free(t_data *data)
 	return (0);
 
 }
+
+void	ft_init_direction(t_data *data, t_ray *ray)
+{
+	if (data->player_spawn_dir == 'S')
+	{
+		ray->dir_x = 0; 
+		ray->dir_y = -1;
+		ray->plane_x = -(double)data->fov / 100; 
+		ray->plane_y = 0;
+	}
+	if (data->player_spawn_dir == 'N')
+	{
+		ray->dir_x = 0; 
+		ray->dir_y = 1;
+		ray->plane_x = (double)data->fov / 100;
+		ray->plane_y = 0;
+	}
+	if (data->player_spawn_dir == 'W')
+	{
+		ray->dir_x = 1; 
+		ray->dir_y = 0;
+		ray->plane_x = 0;
+		ray->plane_y = (double)data->fov / 100; 
+	}
+	if (data->player_spawn_dir == 'E')
+	{
+		ray->dir_x = -1; 
+		ray->dir_y = 0;
+		ray->plane_x = 0;
+		ray->plane_y = -(double)data->fov / 100; 
+	}
+}
+
+void	ft_init_ray_data(t_data *data, t_ray *ray)
+{
+	ray->pos_x = data->player_spawn_pos[0]; 
+	ray->pos_y = data->player_spawn_pos[1]; 
+	ray->map = data->map; 
+	ray->hit = 0; 
+	ray->side = 0; 
+	ray->color = 16711680;
+	ray->resolution_x = data->resolution_x; 
+	ray->resolution_y = data->resolution_y; 
+	ft_init_direction(data, ray);
+	ray->camera_x = 0;
+	ray->raydir_x = 0;
+	ray->raydir_y = 0;
+	ray->side_x = 0;
+	ray->side_y = 0; 
+	ray->delta_x = 0;
+	ray->delta_y = 0;
+	ray->step_x = 0;
+	ray->step_y = 0;
+	
+}
+
 int	main(int argc, char **argv)
 	{
 	(void)argc; 
@@ -96,6 +152,8 @@ int	main(int argc, char **argv)
 	ft_init_display(&data);
 	if (ft_parsing(&data, argv[1]) == 1)
 		return (ft_free(&data));
-	mlx_loop_hook(data.mlx_win, ft_render_next_frame, &data);
+	ft_init_ray_data(&data, &data.ray_data);
+	ft_render_next_frame(&data);
+	//mlx_loop_hook(data.mlx_win, ft_render_next_frame, &data);
 	mlx_loop(data.mlx);
 	}
--- srcs/ray_casting.c
+++ srcs/ray_casting.c
@@ -6,82 +6,13 @@
 /*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:31:47 by ydumaine          #+#    #+#             */
-/*   Updated: 2022/07/09 20:17:42 by ydumaine         ###   ########.fr       */
+/*   Updated: 2022/07/10 18:58:27 by ydumaine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include <unistd.h>
 
-typedef struct ray_data
-{
-	double	resolution_x; 
-	double	camera_x;
-	double	plane_x; // vecteur plan camera
-	double	plane_y;
-	double	dir_x; // vecteur du regard du joueur
-	double	dir_y; 
-	double	raydir_x; // vecteur rayon lanceé pour detecter un mur
-	double	raydir_y; 
-	double	pos_x;	// position du joueur 
-	double	pos_y;
-	int		map_x; // position du carre dans lequel est le joueur sur la carte
-	int		map_y; 
-	double	side_x; //distance a parcourir avant la premiere intersection de la position du joueur 
-	double	side_y;
-	double	delta_x;// distance a parcourir avant une intersection 
-	double	delta_y;
-	int		step_x; // sens du rayon ( positif ou negatif ) x
-	int		step_y; // sens du rayon ( positif ou negatif ) y
-	int		hit;	// indique si un mur a ete touche 
-	int		side =
-	int		walldistance;
-	char **map;
-}
-		t_ray
-
-void	ft_init_direction(t_data *data, t_ray *ray)
-{
-	if (data->player_spawn_dir == 'S')
-	{
-		ray_data->dir_x = 0; 
-		ray_data->dir_y = -1;
-		ray_data->plane_x = -data>fov / 100; 
-		ray_data->plane_y = 0;
-	}
-	if (data->player_spawn_dir == 'N')
-	{
-		ray_data->dir_x = 0; 
-		ray_data->dir_y = 1;
-		ray_data->plane_x = data->fov / 100;
-		ray_data->plane_y = 0;
-	}
-	if (data->player_spawn_dir == 'W')
-	{
-		ray_data->dir_x = 1; 
-		ray_data->dir_y = 0;
-		ray_data->plane_x = 0;
-		ray_data->plane_y = data->fov / 100; 
-	}
-	if (data->player_spawn_dir == 'E')
-	{
-		ray_data->dir_x = -1; 
-		ray_data->dir_y = 0;
-		ray_data->plane_x = 0;
-		ray_data->plane_y = -data->fov / 100; 
-	}
-}
-int ft_launch_ray(t_ray *ray_data)
-void	ft_init_ray_data(t_data *data, t_ray *ray)
-{
-	ray->posx = data->posx; 
-	ray->posy = data->posy; 
-	ray->map = data->map; 
-	rayrdata->resolution_x = data->resolution_x; 
-	ft_init_direction(data, ray)
-
-
-
-}
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char *dst; 
@@ -93,41 +24,43 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 // calcul of ray vector 
 int	ft_render_next_frame(t_data *data)
 {
-	int	x; 
+	int	x;
+	t_ray *ray;
+	ray = &data->ray_data;
 
 	x = 0;
-	ray->map_x = (int)pos_x;
-	ray->map_y = (int)pos_y;
+	ray->map_x = (int)ray->pos_x;
+	ray->map_y = (int)ray->pos_y;
 	while (x < ray->resolution_x)
 	{
 		ray->camera_x = 2 * x / ray->resolution_x - 1;
-		ray->raydir_x =	ray->dir_x + ray->plane_x * ray->camera_x
-		ray->raydir_y =	ray->dir_y + ray->plane_y * ray->camera_x
-		if ray->dir_x = 0;
+		ray->raydir_x =	ray->dir_x + ray->plane_x * ray->camera_x;
+		ray->raydir_y =	ray->dir_y + ray->plane_y * ray->camera_x;
+		if (ray->raydir_x == 0)
 			ray->dir_x = exp(30);
-		if ray->dir_y = 0;
-			ray->dir_y = exp(30);
-		deltay = sqrt(1 + ((ray->raydirx * ray->raydirx ) / ( ray->radiry * ray->raydiry)); // distance a parcourir pour aller a l'intersection suivante
-		deltax = sqrt(1 + ((ray->raydiry * ray->raydiry ) / ( ray->radirx * ray->raydirx));
-		if (ray->raydirx < 0)
+		if (ray->dir_y == 0)
+			ray->raydir_y = exp(30);
+		ray->delta_y = sqrt(1 + ((ray->raydir_x * ray->raydir_x ) / ( ray->raydir_y * ray->raydir_y))); // distance a parcourir pour aller a l'intersection suivante
+		ray->delta_x = sqrt(1 + ((ray->raydir_y * ray->raydir_y ) / ( ray->raydir_x * ray->raydir_x)));
+		if (ray->raydir_x < 0)
 		{
 			ray->step_x = -1;
-			ray->side_x = (ray->pos_x - ray->map_x) * ray->dist_x; 
+			ray->side_x = (ray->pos_x - ray->map_x) * ray->delta_x; 
 		}
 		else
 		{
 			ray->step_x = 1;
-			ray->side_x = (ray->map_x + 1 - ray->pos_x) * ray->dist_x; 
+			ray->side_x = (ray->map_x + 1 - ray->pos_x) * ray->delta_x; 
 		}
-		if (ray->raydiry < 0)
+		if (ray->raydir_y < 0)
 		{
 			ray->step_y = -1;
-			ray->side_y = (ray->pos_y - ray->map_y) * ray->dist_y; 
+			ray->side_y = (ray->pos_y - ray->map_y) * ray->delta_y; 
 		}
 		else
 		{
 			ray->step_y = 1;
-			ray->side_y = (ray->map_y + 1 - ray->pos_y) * ray->dist_y; 
+			ray->side_y = (ray->map_y + 1 - ray->pos_y) * ray->delta_y; 
 		}
 		while (ray->hit == 0) 
 		{
@@ -135,31 +68,36 @@ int	ft_render_next_frame(t_data *data)
 			{
 				ray->side_x += ray->delta_x; 
 				ray->map_x += ray->step_x; 
-				ray->side = 0;
+				ray->side = 0;               
 			}
 			else 
 			{
-				ray->side_y += delta_y;
-				ray->map_y += step_y; 
+				ray->side_y += ray->delta_y;
+				ray->map_y += ray->step_y; 
 				ray->side = 1;
 			}
-			if (ray->map[ray->map_x][ray->map_y] > 0) 
+			if (ray->map[ray->map_y][ray->map_x] == '1')
 				ray->hit = 1; 
 		}
 		if (ray->side == 0)
-			ray->walldistance = (ray->side_x - ray->delta_x)
+			ray->walldistance = (ray->side_x - ray->delta_x); // on prend la distance dx car c'est le rayon x qui a connecte au mur en premier
 		else 
-			ray->walldistance = (ray->side_y - ray->delta_y)
+			ray->walldistance = (ray->side_y - ray->delta_y); // come on est un carre trop loin il faut enlever la distance de 1 carrere
+		ray->lineheight = (int)(ray->resolution_y / ray->walldistance);
+		ray->drawstart = (-ray->lineheight / 2) + (ray->resolution_y / 2);
+		ray->drawend = (ray->lineheight / 2) + (ray->resolution_y / 2);
+		if (ray->drawstart < 0)
+			ray->drawstart = 0;
+		if (ray->drawend >= ray->resolution_y)
+			ray->drawend = ray->resolution_y - 1;
+		while(ray->drawstart < ray->drawend)
+		{
+			my_mlx_pixel_put(data, x, ray->drawstart, ray->color);
+			ray->drawstart++;
+		}
+		x++;
+	
 	}
+	mlx_put_image_to_window(data->mlx, data->mlx_win, data->display, 0, 0);
+	return (0);
 }
-
-int	ft_launch_ray(t_ray ray)
-{
-
-}
-/*
-void ft_init_raycaster(t_data *data)
-{
-
-}
-*/
