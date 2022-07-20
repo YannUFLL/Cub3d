dd77d14577dcdf9887c60bea3c48f126ddd87b1d
Jean-michel Rasser <jrasser@k2r4p4.42mulhouse.fr>
2022-07-21 00:22:43 +0200

    merge

--- Makefile
+++ Makefile
@@ -6,12 +6,12 @@
 #    By: jrasser <jrasser@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/05/17 05:26:46 by jrasser           #+#    #+#              #
-#    Updated: 2022/07/20 23:18:39 by jrasser          ###   ########.fr        #
+#    Updated: 2022/07/20 23:47:26 by jrasser          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-SRCS 		= $(wildcard srcs/*/*.c)
-OBJS		:= $(addprefix objs/, $(notdir $(SRCS:.c=.o)))
+SRCS 		= $(wildcard mandatory/srcs/*/*.c)
+OBJS		:= $(addprefix mandatory/objs/, $(notdir $(SRCS:.c=.o)))
 OS			= ${shell uname}
 LIBMLXMAC	= make -C minilibx/
 NAME 		= cub3d
@@ -21,8 +21,8 @@ CFLAGS		= -Wall -Wextra -Werror
 CPPFLAGS	= -I./include/ -I./libft/ -I./minilibx/
 DEBEUG		= -g -fsanitize=address
 
-objs/%.o: */*/%.c
-			@${CC} -o $@ -c $< ${CFLAGS}
+mandatory/objs/%.o: mandatory/*/*/%.c
+			@${CC} -o $@ -c $< ${CFLAGS} ${DEBEUG}
 
 ifeq ($(OS),Linux)
 LDFLAGS			= -Lmlx_linux -lmlx_Linux -L/usr/lib -lXext -lX11 -lm -lz 
@@ -41,7 +41,7 @@ endif
 
 ${NAME}	:	${OBJS}
 			@$(MAKE) --no-print-directory -C ./libft
-			@${CC} -o ${NAME} ${OBJS} -L./libft -lft $(LDFLAGS)
+			@${CC} -o ${NAME} ${OBJS} -L./libft -lft $(LDFLAGS) ${DEBEUG}
 
 all :		${NAME}
 
---
+++ mandatory/srcs/raycasting/movements_mouse.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movements_mouse.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydumaine <ydumaine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/19 17:54:47 by ydumaine          #+#    #+#             */
+/*   Updated: 2022/07/20 15:38:24 by ydumaine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub3d.h"
+
+void	ft_mouse_rotate_right(t_ray *ray, t_key *key)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+	int		rotate_nb;
+
+	rotate_nb = 0;
+	old_dir_x = ray->dir_x;
+	old_plane_x = ray->plane_x;
+	while (key->mouse_rotate_right > 1)
+	{
+		old_dir_x = ray->dir_x;
+		ray->dir_x = ray->dir_x * cos(-0.005)
+			- ray->dir_y * sin(-0.005);
+		ray->dir_y = old_dir_x * sin(-0.005)
+			+ ray->dir_y * cos(-0.005);
+		old_plane_x = ray->plane_x;
+		ray->plane_x = ray->plane_x * cos(-0.005)
+			- ray->plane_y * sin(-0.005);
+		ray->plane_y = old_plane_x * sin(-0.005)
+			+ ray->plane_y * cos(-0.005);
+	key->mouse_rotate_right--;
+	}
+}
+
+void	ft_mouse_rotate_left(t_ray *ray, t_key *key)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+	int		rotate_nb;
+
+	rotate_nb = 0;
+	old_dir_x = ray->dir_x;
+	old_plane_x = ray->plane_x;
+	while (key->mouse_rotate_left > 1)
+	{
+		old_dir_x = ray->dir_x;
+		ray->dir_x = ray->dir_x * cos(0.005)
+			- ray->dir_y * sin(0.005);
+		ray->dir_y = old_dir_x * sin(0.005)
+			+ ray->dir_y * cos(0.005);
+		old_plane_x = ray->plane_x;
+		ray->plane_x = ray->plane_x * cos(0.005)
+			- ray->plane_y * sin(0.005);
+		ray->plane_y = old_plane_x * sin(0.005)
+			+ ray->plane_y * cos(0.005);
+	key->mouse_rotate_left--;
+	}
+}
